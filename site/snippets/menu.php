df71342c4b2d26a94b861a675e7671a37e5eb945
Jonny Kates <hi@jonnykates.com>
2017-04-03 16:06:59 +0100

    Splitting the books collection in two

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,5 +1,10 @@
 <ul class="main-navigation col--main">
   <li><a href="/about">About</a></li>
-  <li><a href="/books">Books</a></li>
+  <li>Books
+    <ul class="main-navigation__sub-menu">
+      <li><a href="/books/?collection=antiquarian">Antiquarian books</a></li>
+      <li><a href="/books/?collection=reference">Art &amp; architecture reference</a></li>
+    </ul>
+  </li>
   <li><a href="/contact">Contact</a></li>
 </ul>
--- site/templates/books.php
+++ site/templates/books.php
@@ -1,34 +1,67 @@
 <?php snippet('header') ?>
 
 <?php
-  $books = page('books')->children()->visible();
+  $collection = get('collection'); // Get the parameter from the title
+
+  $collectionTitle = "";
+  if($collection === "reference") {
+    $collectionTitle = "Art and Architecture Reference";
+  } else if($collection === "antiquarian") {
+    $collectionTitle = "Antiquarian books";
+  } else {
+    $collectionTitle = "All books";
+  }
+
+  $books = page('books')->children()->visible()->filterBy('collections', $collection, ',')->paginate(20);
 ?>
+
+<h1 class="collection-title"><?php echo $collectionTitle ?></h1>
 
 <div class="books-grid">
 
-  <?php foreach($books->sortBy('author', 'asc') as $book): ?>
-    <a class="books-grid__book <?php if($book->isSold() == '1') { echo 'book-sold'; } ?>" href="<?= $book->url() ?>">
-      <div class="book__image">
+  <?php
+    if($books->count() > 0): ?>
+      <?php foreach($books->sortBy('author', 'asc') as $book): ?>
+        <a class="books-grid__book <?php if($book->isSold() == '1') { echo 'book-sold'; } ?>" href="<?= $book->url() ?>">
+          <div class="book__image">
 
-        <?php
-          if($book->coverImage()->isNotEmpty()) {
-            $coverImage = $book->images()->find($book->coverImage());
-            $coverImageThumb = thumb($coverImage, array('height' => 220))->url();
-            echo "<div class='book__cover' style='background-image: url($coverImageThumb);'></div>";
-          } else {
-            echo "<div class='book__cover book__cover--placeholder'><span>No image</span></div>";
-          }
-        ?>
+            <?php
+              if($book->coverImage()->isNotEmpty()) {
+                $coverImage = $book->images()->find($book->coverImage());
+                $coverImageThumb = thumb($coverImage, array('height' => 220))->url();
+                echo "<div class='book__cover' style='background-image: url($coverImageThumb);'></div>";
+              } else {
+                echo "<div class='book__cover book__cover--placeholder'><span>No image</span></div>";
+              }
+            ?>
 
-      </div>
-      <h4 class="book__author"><?= $book->author()->html() ?></h4>
-      <h5 class="book__title"><?= $book->shortTitle()->html() ?></h5>
-      <?php if($book->isSold() == '1'): ?>
-        <p class="book-sold-label">Sold</p>
-      <?php endif ?>
-    </a>
+          </div>
+          <h4 class="book__author"><?= $book->author()->html() ?></h4>
+          <h5 class="book__title"><?= $book->shortTitle()->html() ?></h5>
+          <?php if($book->isSold() == '1'): ?>
+            <p class="book-sold-label">Sold</p>
+          <?php endif ?>
+        </a>
 
-  <?php endforeach ?>
+      <?php endforeach ?>
+    <?php else: ?>
+      <span class="no-books">No books to show</span>
+    <?php endif ?>
+
 </div>
+
+<?php if($books->pagination()->hasPages()): ?>
+<nav class="pagination">
+
+  <?php if($books->pagination()->hasPrevPage()): ?>
+  <a class="prev" href="<?php echo $books->pagination()->prevPageURL() ?>">&lsaquo; Previous page</a>
+  <?php endif ?>
+
+  <?php if($books->pagination()->hasNextPage()): ?>
+  <a class="next" href="<?php echo $books->pagination()->nextPageURL() ?>">Next page &rsaquo;</a>
+  <?php endif ?>
+
+</nav>
+<?php endif ?>
 
 <?php snippet('footer') ?>
